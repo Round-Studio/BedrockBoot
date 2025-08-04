dfde0b94e55050de3dc4c39291200e4086a27ba8
Mr. Python <hello-python@outlook.com>
2025-08-04 11:35:22 +0800

    使用ItemsRepeater

---
+++ BedrockBoot/Type/VersionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBoot.Type
+{
+    class VersionType
+    {
+        public string Version { get; set; } = "ERROR VERSION";
+        public string Date { get; set; } = "ERROR DATE";
+    }
+}

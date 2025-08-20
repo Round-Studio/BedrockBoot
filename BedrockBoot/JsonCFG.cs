89a68f691ac929cd019684812ff0e35ff46816e6
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-08-20 16:21:05 +0800

    feat: 增加版本目录设置

--- BedrockBoot/JsonCFG.cs
+++ BedrockBoot/JsonCFG.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BedrockBoot.Models.Entry;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,16 @@ namespace BedrockBoot
         public string backimg { get; set; }
         public int DownThread { get; set; } = 12;
         public int DelayTimes { get; set; } = 1000;
+        public List<GameFolderInfoEntry> GameFolders { get; set; } = new List<GameFolderInfoEntry>()
+        {
+            new GameFolderInfoEntry
+            {
+                Path = Path.GetFullPath(".minecraft/Bedrock"),
+                Name = "默认游戏目录",
+                IsAccent = true
+            }
+        };
+        public int ChooseFolderIndex { get; set; } = 0;
         public static JsonCFG FromJson_cfg_base(Json_cfg_base cfg)
         {
             var jsonCfg = new JsonCFG();

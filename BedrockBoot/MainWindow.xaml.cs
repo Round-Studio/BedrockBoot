7c74c5325546c8b120321e24f50b00cd68e5fda7
Minecraft一角钱 <liy_0924@qq.com>
2025-08-26 13:50:37 +0800

    fix: 关闭窗口后无法退出进程

--- BedrockBoot/MainWindow.xaml.cs
+++ BedrockBoot/MainWindow.xaml.cs
@@ -110,7 +110,7 @@ namespace BedrockBoot
         {
             // _hotKeyService?.Dispose();
             // global_cfg.cfg.SaveConfig();
-            // Environment.Exit(0);
+            Environment.Exit(0);
         }
         public async void UpdateBackground()
         {

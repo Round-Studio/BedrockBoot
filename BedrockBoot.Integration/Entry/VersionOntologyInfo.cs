c5a7287f1fffd22eb02073c4e7a557548019ce6e
Minecraft一角钱 <liy_0924@qq.com>
2025-08-31 17:54:43 +0800

    111

---
+++ BedrockBoot.Integration/Entry/VersionOntologyInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace BedrockBoot.Integration.Entry;
+
+public class VersionOntologyInfo
+{
+    public string FolderPath { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string BasePath => Path.Combine(FolderPath, "bedrock_versions", Name);
+}

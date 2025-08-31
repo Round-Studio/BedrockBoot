11ae970e55af5258cde7b6ed60ce65c6ffcb5a65
Minecraft一角钱 <liy_0924@qq.com>
2025-08-31 12:10:48 +0800

    feat

---
+++ BedrockBoot.ProcessMonitor/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace BedrockBoot.ProcessMonitor;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        SuspendLayout();
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.Color.White;
+        ClientSize = new System.Drawing.Size(784, 411);
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        Icon = ((System.Drawing.Icon)resources.GetObject("$this.Icon"));
+        MaximizeBox = false;
+        MaximumSize = new System.Drawing.Size(800, 450);
+        MinimizeBox = false;
+        MinimumSize = new System.Drawing.Size(800, 450);
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        Text = "BedrockBoot 崩溃报告";
+        ResumeLayout(false);
+    }
+
+    #endregion
+}
---
+++ BedrockBoot.ProcessMonitor/Form2.cs
@@ -0,0 +1,12 @@
+﻿namespace BedrockBoot.ProcessMonitor;
+
+public partial class Form2 : Form
+{
+    public Form2()
+    {
+        InitializeComponent();
+        
+        var monitor = new Program.EfficientProcessMonitor();
+        monitor.MonitorSpecificProcess("BedrockBoot");
+    }
+}

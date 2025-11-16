50746fdd01cd89bf863c6fea6c2defdfdbe2667d
Dime <liy_0924@qq.com>
2025-11-16 14:23:31 +0800

    feat: 实例管理页面

---
+++ src/BedrockBoot/Views/Pages/LoadingPage.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BedrockBoot.Views.Pages;
+
+public partial class LoadingPage : UserControl
+{
+    public LoadingPage()
+    {
+        InitializeComponent();
+    }
+}
--- src/BedrockBoot/Views/Windows/MainWindow.axaml.cs
+++ src/BedrockBoot/Views/Windows/MainWindow.axaml.cs
@@ -1,12 +1,46 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Threading;
+using BedrockBoot.Views.Pages;
+using BedrockLauncher.Core;
+using OnePointUI.Avalonia.Base.Entry;
+using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Dialog;
 using OnePointUI.Avalonia.Styling.Controls.OnePointControls.WindowFrame;
 
 namespace BedrockBoot.Views.Windows;
 
 public partial class MainWindow : OnePointWindow
 {
+    public static BedrockCore BedrockCore { get; set; }
     public MainWindow()
     {
         InitializeComponent();
+
+        MainFrame.NavigateTo(new LoadingPage());
+        Task.Run(() =>
+        {
+            BedrockCore = new  BedrockCore();
+            try
+            {
+                BedrockCore.Init();
+                Console.WriteLine("初始化核心完毕");
+            }
+            catch
+            {
+                Console.WriteLine("无法自动打开开发者模式");
+                DialogHost.Show(new DialogInfo()
+                {
+                    Title = "开发者模式",
+                    Content = "我们貌似无法帮您打开开发者模式，请您手动前往设置中打开。",
+                    CloseButtonText = "确定"
+                });
+            }
+            finally
+            {
+                Console.WriteLine("跳转主页面.jpg");
+                Dispatcher.UIThread.Invoke(() => MainFrame.NavigateTo(new MainPage()));
+            }
+        });
     }
 }

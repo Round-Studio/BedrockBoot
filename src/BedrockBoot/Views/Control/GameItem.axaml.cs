eee973d83c25ade0955a199b49fb5306864b028b
Dime <liy_0924@qq.com>
2025-11-16 15:49:41 +0800

    feat: 基础的启动游戏

--- src/BedrockBoot/Views/Control/GameItem.axaml.cs
+++ src/BedrockBoot/Views/Control/GameItem.axaml.cs
@@ -1,7 +1,12 @@
-﻿using Avalonia;
+﻿using System;
+using Windows.Foundation;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using BedrockBoot.Base.Entry.Game;
+using BedrockBoot.Views.Windows;
+using BedrockLauncher.Core;
 
 namespace BedrockBoot.Views.Control;
 
@@ -25,5 +30,21 @@ public partial class GameItem : UserControl
     {
         Card.Header = VersionInfo.VersionName;
         Card.Description = $"{VersionInfo.Type} {VersionInfo.RealVersion}";
+    }
+
+    private void LaunchBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        MainWindow.BedrockCore.ChangeVersion(VersionInfo.Version_Path,new InstallCallback()
+        {
+            result_callback = new Action<AsyncStatus, Exception>((s,e) =>
+            {
+                
+            }),
+            registerProcess_percent = new Action<string, uint>((s, e) =>
+            {
+                
+            })
+        });
+        MainWindow.BedrockCore.LaunchGame(VersionType.Release);
     }
 }

212151fa2638a1c7ac7e9e02d97910f936f1fe67
Dime <liy_0924@qq.com>
2025-11-16 09:21:25 +0800

    feat: Add Core Project

--- src/BedrockBoot/Views/Pages/MainPage.axaml.cs
+++ src/BedrockBoot/Views/Pages/MainPage.axaml.cs
@@ -7,10 +7,38 @@ namespace BedrockBoot.Views.Pages;
 
 public partial class MainPage : UserControl
 {
+    public bool IsEditMode { get; set; } = false;
     public MainPage()
     {
         InitializeComponent();
 
         MainFrame.NavigateTo(new MainHomePage());
+
+        IsEditMode = true;
+    }
+
+    private void SelTag_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (IsEditMode)
+        {
+            var item = (TabItem)SelTag.SelectedItem;
+            var tag = item.Tag as string;
+
+            switch (tag)
+            {
+                case "Home":
+                    MainFrame.NavigateTo(new MainHomePage());
+                    break;
+                case "Manager":
+                    MainFrame.NavigateTo(new MainManager());
+                    break;
+                case "Download":
+                    MainFrame.NavigateTo(new MainDownloadPage());
+                    break;
+                case "Setting":
+                    MainFrame.NavigateTo(new MainSettingPage());
+                    break;
+            }
+        }
     }
 }

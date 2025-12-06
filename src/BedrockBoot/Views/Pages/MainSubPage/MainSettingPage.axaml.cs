acdcabcf7f4a351b39f8bd549555cba9b9674ef6
Dime <liy_0924@qq.com>
2025-12-07 00:43:06 +0800

    feat: 更丰富的设置页面

--- src/BedrockBoot/Views/Pages/MainSubPage/MainSettingPage.axaml.cs
+++ src/BedrockBoot/Views/Pages/MainSubPage/MainSettingPage.axaml.cs
@@ -1,10 +1,13 @@
-﻿using Avalonia;
+﻿using System;
+using System.Reflection;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using BedrockBoot.Base.Entry;
 using BedrockBoot.Views.Pages.SettingSubPage;
 using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Navigation;
 using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Navigation.Breadcrumb;
+using Round.SDK.Helper;
 
 namespace BedrockBoot.Views.Pages.MainSubPage;
 
@@ -15,6 +18,8 @@ public partial class MainSettingPage : BedrockBootPage
     public MainSettingPage()
     {
         InitializeComponent();
+        BuildTime.Text =
+            $"Build.2.{((DateTime)(CheckVersion.GetBuildTimestamp(Assembly.GetExecutingAssembly()))).ToString("yy.MMdd.HHmmss")}";
         SettingBreadcrumbBar = this.BreadcrumbBar;
         this.BreadcrumbBar.RootItemClick = () =>
             SettingFrame.NavigateTo(new SettingNavigation());

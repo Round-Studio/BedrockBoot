907c79a7a1c8976ebd3aa63b54db1c32e4f983f4
Dime <liy_0924@qq.com>
2025-12-21 17:24:24 +0800

    feat: 添加页面

--- src/BedrockBoot/Views/Pages/SettingSubPage/SettingNavigation.axaml.cs
+++ src/BedrockBoot/Views/Pages/SettingSubPage/SettingNavigation.axaml.cs
@@ -2,6 +2,7 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using BedrockBoot.Models.Global;
 using BedrockBoot.Views.Pages.MainSubPage;
 using BedrockBoot.Views.Pages.OtherPage;
 
@@ -12,6 +13,10 @@ public partial class SettingNavigation : UserControl
     public SettingNavigation()
     {
         InitializeComponent();
+
+#if RELEASE
+        SetPersonalization.IsEnabled = GlobalModel.FunctionOption.IsEnableSettingPersonalization;
+#endif
     }
 
     private void AboutUs_OnClick(object? sender, RoutedEventArgs e)
@@ -27,5 +32,10 @@ public partial class SettingNavigation : UserControl
     private void Universal_OnClick(object? sender, RoutedEventArgs e)
     {
         MainSettingPage.NavigationFrame.NavigateTo(new SettingUniversal());
+    }
+
+    private void SetPersonalization_OnClick(object? sender, RoutedEventArgs e)
+    {
+        MainSettingPage.NavigationFrame.NavigateTo(new SettingPersonalization());
     }
 }

a7659e64ede927c07ae8aaaca0d5b9e5bfc23cb8
Dime <liy_0924@qq.com>
2025-12-21 09:47:14 +0800

    feat: 修改错误信息窗口

--- src/BedrockBoot/Views/Windows/ExceptionWindow.axaml.cs
+++ src/BedrockBoot/Views/Windows/ExceptionWindow.axaml.cs
@@ -1,5 +1,8 @@
-﻿using Avalonia;
+﻿using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input.Platform;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using OnePointUI.Avalonia.Styling.Controls.OnePointControls.WindowFrame;
 
@@ -17,5 +20,28 @@ public partial class ExceptionWindow : OnePointWindow
     {
         Log = logs;
         LogBox.Text = logs;
+    }
+
+    private void GithubBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "https://github.com/Round-Studio/BedrockBoot/issues",
+            UseShellExecute = true
+        });
+    }
+
+    private async void CopyButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var clipboard = this.Clipboard;
+        if (clipboard != null)
+        {
+            await clipboard.SetTextAsync(Log);
+        }
+    }
+
+    private void CloseButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }

4b285589a1f25942ecc987bdcccff54af030b431
Dime <liy_0924@qq.com>
2026-01-01 01:20:07 +0800

    feat: 更新清单

--- src/BedrockBoot/Base/Entry/Manifest/FunctionOptionEntry.cs
+++ src/BedrockBoot/Base/Entry/Manifest/FunctionOptionEntry.cs
@@ -6,6 +6,7 @@ public class FunctionOptionEntry
 {
     [JsonPropertyName("isEnableImportGamePack")] public bool IsEnableImportGamePack { get; set; }
     [JsonPropertyName("isEnableGameInstanceControl")] public bool IsEnableGameInstanceControl { get; set; }
+    [JsonPropertyName("isEnableGameInstanceMods")] public bool IsEnableGameInstanceMods { get; set; }
     [JsonPropertyName("isEnableWebProtocol")] public bool IsEnableWebProtocol { get; set; }
     [JsonPropertyName("isEnableInstanceIndependent")] public bool IsEnableInstanceIndependent { get; set; }
     [JsonPropertyName("isEnableSettingPersonalization")] public bool IsEnableSettingPersonalization { get; set; }

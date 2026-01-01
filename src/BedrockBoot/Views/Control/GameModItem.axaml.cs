cc72bad1efedc531ce8f87b19361b3237d027e51
Dime <liy_0924@qq.com>
2026-01-01 13:32:18 +0800

    feat: 添加模组管理页面

---
+++ src/BedrockBoot/Views/Control/GameModItem.axaml.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using BedrockBoot.Base.Entry.Game.Pack.Mods;
+
+namespace BedrockBoot.Views.Control;
+
+public partial class GameModItem : UserControl
+{
+    public ModInfo ModInfo { get; set; }
+
+    public GameModItem()
+    {
+        InitializeComponent();
+    }
+
+    public GameModItem(ModInfo info) : this()
+    {
+        ModInfo = info;
+        
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        Card.Header = Path.GetFileName(ModInfo.File);
+        Card.Description = $"{FormatBytes(new FileInfo(ModInfo.File).Length)}，{ModInfo.InjectDelay} ms";
+    }    
+    static string FormatBytes(double bytes)
+    {
+        string[] suffixes = { "B", "KB", "MB", "GB", "TB" };
+        int counter = 0;
+        double number = bytes;
+
+        while (number >= 1024 && counter < suffixes.Length - 1)
+        {
+            number /= 1024;
+            counter++;
+        }
+
+        return $"{number:F1} {suffixes[counter]}";
+    }
+}
--- src/BedrockBoot/Views/Pages/InstanceSubPage/DrawContent/InstanceMods.axaml.cs
+++ src/BedrockBoot/Views/Pages/InstanceSubPage/DrawContent/InstanceMods.axaml.cs
@@ -1,9 +1,18 @@
-﻿using Avalonia;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Documents;
+using System.Windows.Shapes;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using BedrockBoot.Base.Entry;
 using BedrockBoot.Base.Entry.Game;
+using BedrockBoot.Base.Entry.Game.Pack.Mods;
 using BedrockBoot.Interface;
 using BedrockBoot.Models.Pack.Game.Mods;
+using BedrockBoot.Views.Control;
+using Path = System.IO.Path;
 
 namespace BedrockBoot.Views.Pages.InstanceSubPage.DrawContent;
 
@@ -11,6 +20,7 @@ public partial class InstanceMods : ISetting
 {
     public VersionConfig VersionInfo { get; set; }
     public ModsManager ModsManager { get; set; }
+    private string _searchKey => SearchBox.Text;
     public InstanceMods()
     {
         IsEdit = false;
@@ -28,6 +38,44 @@ public partial class InstanceMods : ISetting
 
     private void UpdateUI()
     {
-        ModsManager.RefreshMods();
+        IsEdit = false;
+        NullBox.IsVisible = false;
+        ResultBox.Children.Clear();
+        var mods = ModsManager.RefreshMods();
+        var resultMods = new List<ModInfo>();
+
+        mods.ForEach(info =>
+        {
+            if (string.IsNullOrEmpty(_searchKey) ||
+                info.File.Contains(_searchKey))
+            {
+                resultMods.Add(info);
+            }
+        });
+
+        if (resultMods.Count <= 0)
+        {
+            NullBox.IsVisible = true;
+        }
+        else
+        {
+            resultMods.ForEach(info =>
+            {
+                ResultBox.Children.Add(new GameModItem(info));
+            });
+        }
+
+        IsEdit = true;
+    }
+
+    private void SearchBox_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (IsEdit)
+            UpdateUI();
+    }
+
+    private void FolderBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Process.Start("explorer", new[] { Path.Combine(VersionInfo.VersionPath, "config", "BedrockBoot2", "mods") });
     }
 }

56185d16779917aaf7298cdc08dec61bfd457061
Dime <liy_0924@qq.com>
2026-01-24 22:20:04 +0800

    feat: 删除无用代码

--- src/BedrockBoot/Views/TaskItem/TaskLaunchGameItem.axaml.cs
+++ src/BedrockBoot/Views/TaskItem/TaskLaunchGameItem.axaml.cs
@@ -145,41 +145,6 @@ public partial class TaskLaunchGameItem : UserControl
         });
     }
 
-    private async Task WaitForProcessExitAsync(Process process)
-    {
-        try
-        {
-            await process.WaitForExitAsync(_cancellationTokenSource.Token);
-
-            Console.WriteLine($@"游戏进程 PID：{MinecraftProcess.Id} 已退出");
-            // 进程正常退出
-            Console.WriteLine($@"进程已退出，退出代码: {process.ExitCode}");
-            Dispatcher.UIThread.Post(() => LaunchCompleted?.Invoke());
-        }
-        catch (TaskCanceledException)
-        {
-            // 用户取消了等待
-            if (!process.HasExited)
-            {
-                process.Kill();
-            }
-        }
-    }
-
-    private void OnProcessExited(object sender, EventArgs e)
-    {
-        Console.WriteLine($@"进程已退出 (事件触发)，退出代码: {MinecraftProcess?.ExitCode}");
-
-        // 确保在UI线程调用回调
-        Dispatcher.UIThread.Post(() => { LaunchCompleted?.Invoke(); });
-
-        // 清理事件处理器
-        if (MinecraftProcess != null)
-        {
-            MinecraftProcess.Exited -= OnProcessExited;
-        }
-    }
-
     public static void Launch(VersionConfig gameInfo)
     {
         GlobalModel.MainWindow.Notice.AddNotice(new NoticeInfo()
@@ -221,38 +186,5 @@ public partial class TaskLaunchGameItem : UserControl
 
         // 调用完成回调
         LaunchCompleted?.Invoke();
-    }
-
-    // 添加 Process 的 WaitForExitAsync 扩展方法
-    public static class ProcessExtensions
-    {
-        public static Task WaitForExitAsync(Process process, CancellationToken cancellationToken = default)
-        {
-            var tcs = new TaskCompletionSource<bool>();
-
-            process.EnableRaisingEvents = true;
-            process.Exited += OnExited;
-
-            if (process.HasExited)
-            {
-                tcs.TrySetResult(true);
-            }
-
-            cancellationToken.Register(() =>
-            {
-                if (!process.HasExited)
-                {
-                    tcs.TrySetCanceled(cancellationToken);
-                }
-            });
-
-            return tcs.Task;
-
-            void OnExited(object sender, EventArgs e)
-            {
-                process.Exited -= OnExited;
-                tcs.TrySetResult(true);
-            }
-        }
     }
 }

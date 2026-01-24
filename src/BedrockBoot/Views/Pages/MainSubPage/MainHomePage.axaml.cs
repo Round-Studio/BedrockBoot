b59f5af825caa23e1460c22fab4a67172394528f
Dime <liy_0924@qq.com>
2026-01-24 17:52:54 +0800

    chore(using): 删除无用引用

--- src/BedrockBoot/Views/Pages/MainSubPage/MainHomePage.axaml.cs
+++ src/BedrockBoot/Views/Pages/MainSubPage/MainHomePage.axaml.cs
@@ -1,20 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-using BedrockBoot.Base.Entry;
-using BedrockBoot.Models.Global;
-using BedrockBoot.Models.Helper;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Avalonia.Interactivity;
-using BedrockBoot.Base.Entry.Game;
-using BedrockBoot.Models.Pack.Game.Import;
-using BedrockBoot.Views.DrawContent;
-using BedrockBoot.Views.TaskItem;
-using OnePointUI.Avalonia.Base.Entry;
-using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Dialog;
+﻿using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Dialog;
 
 namespace BedrockBoot.Views.Pages.MainSubPage;
 

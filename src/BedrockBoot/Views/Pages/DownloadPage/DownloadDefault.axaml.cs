bd0e8b7d21c8abd5db94af14aafd55e63c29bf56
Dime <liy_0924@qq.com>
2026-01-27 21:05:49 +0800

    feat: 新的下载页面

---
+++ src/BedrockBoot/Views/Pages/DownloadPage/DownloadDefault.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BedrockBoot.Views.Pages.DownloadPage;
+
+public partial class DownloadDefault : UserControl
+{
+    public DownloadDefault()
+    {
+        InitializeComponent();
+    }
+}

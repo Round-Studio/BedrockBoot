0321a98439afd8c85e59abc2a608d88de31ca561
Dime <liy_0924@qq.com>
2026-02-01 00:12:49 +0800

    refactor: 重构存档管理页面

--- src/BedrockBoot/Views/Control/Items/ArchiveItem.axaml.cs
+++ src/BedrockBoot/Views/Control/Items/ArchiveItem.axaml.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
+using BedrockBoot.Base.Entry;
 using BedrockBoot.Base.Entry.Game.Pack.Archive;
+using BedrockBoot.Models.Global;
+using OnePointUI.Avalonia.Base.Entry;
 using Round.SDK.Helper;
 
 namespace BedrockBoot.Views.Control.Items;
@@ -41,6 +48,46 @@ public partial class ArchiveItem : UserControl
                 Stretch = Stretch.UniformToFill,
                 Source = new Bitmap(ArchiveInfo.IconPath)
             };
+        }
+    }
+
+    private void OpenFolderBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Console.WriteLine($@"{Directory.Exists(ArchiveInfo.Path)}");
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = ArchiveInfo.Path,
+            UseShellExecute = true
+        });
+    }
+
+    private async void SaveBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) return;
+        
+        var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+        {
+            Title = "导出 Minecraft World Pack",
+            DefaultExtension = "mcworld",
+            FileTypeChoices = new[]
+            {
+                new FilePickerFileType("MCWORLD (*.mcworld)")
+                {
+                    Patterns = new[] { "*.mcworld" }
+                }
+            }
+        });
+        
+        if (file != null)
+        {
+            ArchiveInfo.Save(file.TryGetLocalPath());
+            GlobalModel.MainWindow.Notice.AddNotice(new NoticeInfo()
+            {
+                Title = "成功",
+                Message = "存档已导出！",
+                NoticeType = NoticeType.Info
+            });
         }
     }
 }

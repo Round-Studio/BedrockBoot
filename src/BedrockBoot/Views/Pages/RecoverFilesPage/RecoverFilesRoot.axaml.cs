38a766a4510948af618e6a2f21dd56211fef481b
Dime <liy_0924@qq.com>
2026-02-05 10:06:41 +0800

    feat: 添加找回工具欢迎页面

---
+++ src/BedrockBoot/Views/Pages/RecoverFilesPage/RecoverFilesRoot.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using OnePointUI.Avalonia.Base.Enum;
+using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Navigation;
+
+namespace BedrockBoot.Views.Pages.RecoverFilesPage;
+
+public partial class RecoverFilesRoot : UserControl
+{
+    public static NavigationFrame NavigationFrameStatic { get; set; }
+
+    public RecoverFilesRoot()
+    {
+        InitializeComponent();
+        NavigationFrameStatic = this.NavigationFrame;
+        NavigationFrameStatic.NavigateTo(new RecoverFilesWellcom());
+    }
+}

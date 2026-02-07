869f0b2fce17962bce3e99f34b44fb64f026bcc1
Dime <liy_0924@qq.com>
2026-02-07 16:50:11 +0800

    feat: 移除测试版提示

--- src/BedrockBoot/Views/Windows/MainWindow.axaml.cs
+++ src/BedrockBoot/Views/Windows/MainWindow.axaml.cs
@@ -160,35 +160,6 @@ public partial class MainWindow : BedrockBootWindow
                     Console.WriteLine(@"跳转主页面.jpg");
                     Dispatcher.UIThread.Invoke(() => MainFrame.NavigateTo(new MainPage()));
                 }
-
-#if DEBUG
-                var date = CheckVersion.GetBuildTimestamp(Assembly.GetExecutingAssembly());
-                var zt = CheckVersion.CheckTimeAndExecute24Hour((DateTime)date);
-                Console.WriteLine(@"当前模式：Debug 模式");
-                Console.WriteLine(@$"当前程序集编译日期：{date}");
-                Console.WriteLine(@$"当前版本是否在可用时间段内：{zt}");
-
-                if (zt)
-                    DialogHost.Show(new DialogInfo
-                    {
-                        Content =
-                            $"当前版本为预览版本，请勿添加到整合包中使用。\n当前版本仅作为测试部分功能，将于 24h 后失效，请抓紧时间进行测试。\n当前可用状态：{zt}",
-                        Title = "版本模式提示",
-                        CloseButtonText = "开始测试",
-                        AccountButton = DialogButtons.CloseButton
-                    });
-                else
-                    DialogHost.Show(new DialogInfo
-                    {
-                        Content =
-                            $"当前版本为预览版本，请勿添加到整合包中使用。\n当前版本仅作为测试部分功能，将于 24h 后失效，当前已失效。\n当前可用状态：{zt}",
-                        Title = "版本模式提示",
-                        CloseButtonText = "退出",
-                        CloseAction = () => { Environment.Exit(0); },
-                        AccountButton = DialogButtons.CloseButton
-                    });
-#endif
-
                 if (!GlobalModel.Config.Data.IsAgreeTerms)
                     DialogHost.Show(new DialogInfo
                     {

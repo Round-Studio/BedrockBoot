60eff26e0aee985a6a07c18fe85e27faa5dafc9f
Dime <liy_0924@qq.com>
2026-02-15 22:55:14 +0800

    feat: 完善联机依赖下载

--- src/BedrockBoot/Views/DialogContent/DialogDownloadMultiPlayerDependenceContent.axaml.cs
+++ src/BedrockBoot/Views/DialogContent/DialogDownloadMultiPlayerDependenceContent.axaml.cs
@@ -1,6 +1,19 @@
-﻿using Avalonia;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using BedrockBoot.Base.Entry.Progress;
+using BedrockBoot.Core.Models.Download;
+using BedrockBoot.Models.Global;
+using BedrockBoot.Views.Pages.MainSubPage;
+using BedrockBoot.Views.Pages.MultiplayerPage;
+using Octokit;
+using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Dialog;
+using Round.SDK.Helper;
 
 namespace BedrockBoot.Views.DialogContent;
 
@@ -9,10 +22,40 @@ public partial class DialogDownloadMultiPlayerDependenceContent : UserControl
     public DialogDownloadMultiPlayerDependenceContent()
     {
         InitializeComponent();
+        Download();
     }
 
     public void Download()
     {
-        
+        Task.Run(async () =>
+        {
+            // 创建客户端
+            var github = new GitHubClient(new ProductHeaderValue("BedrockBoot"));
+
+            // 获取指定仓库的所有发布
+            var owner = "EasyTier";
+            var repo = "EasyTier";
+            var releases = (await github.Repository.Release.GetLatest(owner, repo)).Assets.First(x => x.Name.Contains("easytier-windows-x86_64"));
+
+            var url = releases.BrowserDownloadUrl;
+            var downloader = new GithubFilesDownload();
+            await downloader.DownloadAsync(url, Path.Combine(PathsList.TempPath, releases.Name),
+                new Progress<DownloadProgress>(p =>
+                {
+                    Dispatcher.UIThread.Invoke(() =>
+                    {
+                        if (ProgressBar.IsIndeterminate)
+                            ProgressBar.IsIndeterminate = false;
+
+                        ProgressBar.Value = (int)p.ProgressPercentage;
+                        ProgressText.Text = $"下载依赖 {p.ProgressPercentage:F2} %";
+                    });
+                }));
+
+            ZipHelper.ExtractZipFile(Path.Combine(PathsList.TempPath, releases.Name), PathsList.EasyTierPath, true);
+            
+            Dispatcher.UIThread.Invoke(DialogHost.Close);
+            Dispatcher.UIThread.Invoke(()=>MainMultiplayerPage.NavigationFrame.NavigateTo(new MultiplayerRoot()));
+        });
     }
 }
--- src/BedrockBoot/Views/Pages/MainSubPage/MainMultiplayerPage.axaml.cs
+++ src/BedrockBoot/Views/Pages/MainSubPage/MainMultiplayerPage.axaml.cs
@@ -1,17 +1,31 @@
-﻿using Avalonia;
+﻿using System.IO;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using BedrockBoot.Base.Entry;
+using BedrockBoot.Models.Global;
 using BedrockBoot.Views.Pages.MultiplayerPage;
+using OnePointUI.Avalonia.Styling.Controls.OnePointControls.Navigation;
 
 namespace BedrockBoot.Views.Pages.MainSubPage;
 
 public partial class MainMultiplayerPage : BedrockBootPage
 {
+    public static NavigationFrame NavigationFrame;
     public MainMultiplayerPage()
     {
         InitializeComponent();
-        
-        MainFrame.NavigateTo(new MultiplayerDependenceDownload());
+
+        NavigationFrame = MainFrame;
+
+        if (!File.Exists(Path.Combine(PathsList.EasyTierPath, "easytier-windows-x86_64", "easytier-core.exe")) ||
+            !File.Exists(Path.Combine(PathsList.EasyTierPath, "easytier-windows-x86_64", "easytier-cli.exe")))
+        {
+            MainFrame.NavigateTo(new MultiplayerDependenceDownload());
+        }
+        else
+        {
+            MainFrame.NavigateTo(new MultiplayerRoot());
+        }
     }
 }

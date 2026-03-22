5fa501234bf46de2cc3fe755029e1c4b05d76359
Dime <liy_0924@qq.com>
2026-03-22 13:48:43 +0800

    feat: 修改默认列表源

--- src/BedrockBoot.Base/Entry/ConfigEntry.cs
+++ src/BedrockBoot.Base/Entry/ConfigEntry.cs
@@ -11,7 +11,7 @@ public class ConfigEntry
     [JsonPropertyName("gameFolders")] public List<GameFolderInfo> GameFolders { get; set; } = new();
     [JsonPropertyName("gameFolderSelIndex")] public int GameFolderSelIndex { get; set; } = -1;
     [JsonPropertyName("downloadChunkCount")] public int DownloadChunkCount { get; set; } = 4;
-    [JsonPropertyName("versionSourceIndex")] public int VersionSourceIndex { get; set; } = 3;
+    [JsonPropertyName("versionSourceIndex")] public int VersionSourceIndex { get; set; } = 0;
     [JsonPropertyName("curseForgeSourceIndex")] public int CurseForgeSourceIndex { get; set; } = 0;
     [JsonPropertyName("styleConfig")] public StyleConfig StyleConfig { get; set; } = new();
     [JsonPropertyName("homeConfig")] public HomeConfig HomeConfig { get; set; } = new();

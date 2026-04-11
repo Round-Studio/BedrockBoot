d27972baa72754da521cc3ac3763c097843870c0
dime <liy_0924@qq.com>
2026-04-12 00:08:03 +0800

    fix: 修复未 using 导致的 bug

--- src/BedrockBoot/Views/TaskItem/TaskDownloadUpdateFileItem.axaml.cs
+++ src/BedrockBoot/Views/TaskItem/TaskDownloadUpdateFileItem.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Controls;

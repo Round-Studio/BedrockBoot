ea655dc4b7e1a5ddfb1730899bc19a16f3460f99
Dime <liy_0924@qq.com>
2026-04-18 14:39:41 +0800

    feat: 添加新的 Github 加速源

--- src/BedrockBoot.Core/Global/SourceList.cs
+++ src/BedrockBoot.Core/Global/SourceList.cs
@@ -14,7 +14,7 @@ public class SourceList
     {
         { "Github", "{url}" },
         { "加速源 ①", "https://github1.roundstudio.top/{url}" },
-        { "llkk.cc", "https://gh.llkk.cc/{url}" },
+        { "gh-proxy.com", "https://gh-proxy.com/{url}" },
         { "gh-proxy.net", "https://gh-proxy.net/{url}" },
         /*{ "", "{route}" }*/
     };

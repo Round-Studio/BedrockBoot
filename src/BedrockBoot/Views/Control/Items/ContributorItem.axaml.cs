16b1c60341ed39e14512e9830c47aacddd754b2d
Dime <liy_0924@qq.com>
2026-05-09 21:41:53 +0800

    feat: 添加贡献者主页跳转

--- src/BedrockBoot/Views/Control/Items/ContributorItem.axaml.cs
+++ src/BedrockBoot/Views/Control/Items/ContributorItem.axaml.cs
@@ -1,15 +1,20 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 using Octokit;
 
 namespace BedrockBoot.Views.Control.Items;
 
 public partial class ContributorItem : UserControl
 {
-    public ContributorItem(RepositoryContributor con)
+    public ContributorItem()
     {
         InitializeComponent();
-
-        NameText.Text = con.Login;
+    }
+    
+    public ContributorItem(RepositoryContributor con):this()
+    {
+        NameText.Content = con.Login;
+        NameText.NavigateUri = new Uri(con.HtmlUrl);
         ContributorText.Text = $"{con.Contributions}";
         IconBox.Update(con.AvatarUrl);
     }

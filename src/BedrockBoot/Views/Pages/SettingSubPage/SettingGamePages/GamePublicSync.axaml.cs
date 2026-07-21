e91885dc67b935d0c4ae03b2137b8d3a3fd900e2
Dime <liy_0924@qq.com>
2026-07-21 19:45:43 +0800

    feat: 在设置页面添加重置同步源的选项

--- src/BedrockBoot/Views/Pages/SettingSubPage/SettingGamePages/GamePublicSync.axaml.cs
+++ src/BedrockBoot/Views/Pages/SettingSubPage/SettingGamePages/GamePublicSync.axaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using BedrockBoot.Core.Global;
 using BedrockBoot.Interface;
 using BedrockBoot.Views.Pages.MainSubPage;
 using OnePointUI.Avalonia.Base.Entry;
@@ -26,5 +28,11 @@ public partial class GamePublicSync : ISettingPage
                 ItemName = "全局配置"
             }
         };
+    }
+
+    private void ResetBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        GlobalModel.Config.Data.PublicOptionsConfig = null;
+        GlobalModel.Config.Save();
     }
 }

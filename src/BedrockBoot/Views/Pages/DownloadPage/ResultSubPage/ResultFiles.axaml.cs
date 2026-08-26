10c12d40189e5f2789f92144631e04acf11051f4
Dime <liy_0924@qq.com>
2026-08-26 14:12:25 +0800

    feat: CurseForge 的文件列表

--- src/BedrockBoot/Views/Pages/DownloadPage/ResultSubPage/ResultFiles.axaml.cs
+++ src/BedrockBoot/Views/Pages/DownloadPage/ResultSubPage/ResultFiles.axaml.cs
@@ -1,7 +1,13 @@
-﻿using Avalonia;
+﻿using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using BedrockBoot.Base.Entry.Game.Pack.ResourcePack.CurseForge;
+using BedrockBoot.Base.Enum.Search;
 using BedrockBoot.Interface.Download;
+using BedrockBoot.Models.Global;
+using BedrockBoot.Models.Pack.Game.ResourcePack.CurseForge;
+using BedrockBoot.Views.Control.Items;
 
 namespace BedrockBoot.Views.Pages.DownloadPage.ResultSubPage;
 
@@ -17,5 +23,17 @@ public partial class ResultFiles : UserControl
     public ResultFiles(IDownloadResult service) : this()
     {
         _service = service;
+        UpdateUI();
+    }
+
+    private async Task UpdateUI()
+    {
+        if (_service.SearchInfo.ResourceType == SearchResourceType.ResourcePack)
+        {
+            var files = await new CurseForgeApiClient(GlobalKeys.CurseForgeApiKey)
+                .GetModFilesAsync(_service.SearchInfo.Id);
+
+            files.Data.ForEach(f => { FilesList.Children.Add(new CurseForgeModBuildFileItem(f)); });
+        }
     }
 }

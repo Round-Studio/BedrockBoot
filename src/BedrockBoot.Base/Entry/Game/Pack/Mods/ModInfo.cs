f32cfd7e9107ac4e049221202bac6f5c8f3073a9
Dime <liy_0924@qq.com>
2026-08-30 11:10:20 +0800

    chore: 删除进程远程注入相关代码

--- src/BedrockBoot.Base/Entry/Game/Pack/Mods/ModInfo.cs
+++ src/BedrockBoot.Base/Entry/Game/Pack/Mods/ModInfo.cs
@@ -11,15 +11,4 @@ public class ModInfo
     [JsonPropertyName("file")] public string File { get; set; }
     [JsonPropertyName("injectDelay")] public int InjectDelay { get; set; } = 0;
     [JsonPropertyName("isPreLoad")] public bool IsPreLoad { get; set; } = false;
-
-    public void Inject(int processId)
-    {
-        if (IsPreLoad)
-            throw new Exception("This mod is PreLoad mod.");
-
-        BedrockBoot.Inject.Native.Init(
-            Dependence.Dependence.GetResource("BedrockBoot.Dependence.Dependence.Inject.dll"));
-        BedrockBoot.Inject.Native.LoadPlugins(processId, Path.GetFullPath(File), InjectDelay != 0,
-            InjectDelay);
-    }
 }

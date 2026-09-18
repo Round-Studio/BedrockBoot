9cba116433c83d02df56cd69791b1e886da47e06
Dime <liy_0924@qq.com>
2026-09-19 02:00:41 +0800

    chore: 删除废弃的 Loading Page

--- src/BedrockBoot/Views/Pages/SetupPage/SetupCompleted.axaml.cs
+++ src/BedrockBoot/Views/Pages/SetupPage/SetupCompleted.axaml.cs
@@ -34,7 +34,7 @@ public partial class SetupCompleted : UserControl
     private void Start_OnClick(object? sender, RoutedEventArgs e)
     {
         Console.WriteLine(@"跳转主页面.jpg");
-        Dispatcher.UIThread.Invoke(() => GlobalModel.MainWindow.MainFrame.NavigateTo(new NeoMainPage()));
+        Dispatcher.UIThread.Invoke(() => GlobalModel.MainWindow.MainFrame.NavigateTo(new MainPage()));
         Core.Global.GlobalModel.Config.Data.IsFirstRun = false;
         Core.Global.GlobalModel.Config.Save();
     }
